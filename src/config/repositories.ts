96c3e7e7256eef42b7baf9a88e782468ebf835f1
MrT <turgeon.dev@gmail.com>
2025-01-15 22:16:48 -0700

    0.1.1-SidebarMenu(V1)-AddedLanguagesSection

--- src/config/repositories.ts
+++ src/config/repositories.ts
@@ -7,6 +7,7 @@ export interface Repository {
 interface RepositoryGroups {
   tools: Repository[];
   topics: Repository[];
+  languages: Repository[];
 }
 
 export const repositories: RepositoryGroups = {
@@ -58,5 +59,22 @@ export const repositories: RepositoryGroups = {
       url: "https://www.youtube.com/watch?v=xvFZjo5PgG0",
       description: "...two related branches of mathematics that deal with the analysis and interpretation of data. Probability is the study of chance and randomness, while statistics is the study of how to collect, organize, and analyze data."
     },
+  ],
+  languages: [
+    {
+      name: "JavaScript",
+      url: "https://github.com/Learn-Like-Me-LLM/JavaScript",
+      description: "A high-level, interpreted programming language that conforms to the ECMAScript specification"
+    },
+    {
+      name: "Python",
+      url: "https://github.com/Learn-Like-Me-LLM/Python",
+      description: "A high-level programming language known for its simplicity and readability"
+    },
+    {
+      name: "Zig",
+      url: "https://github.com/Learn-Like-Me-LLM/Zig",
+      description: "A general-purpose programming language designed for robustness, optimality, and maintainability"
+    }
   ]
 } 
